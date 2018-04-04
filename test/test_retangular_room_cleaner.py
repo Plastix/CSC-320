d1a08a39bdffc9796f6e6a8fa09f721329a84956
Plastix <aidanpieper@gmail.com>
2018-04-04 14:54:24 -0400

    Add unit tests for RectangularRoomCleaner

---
+++ test/test_retangular_room_cleaner.py
@@ -0,0 +1,31 @@
+import unittest
+import pacman
+
+
+class TestRectangularRoomCleaner(unittest.TestCase):
+    layouts = [
+        ['rectangularRoom-1'],
+        ['rectangularRoom-2'],
+        ['rectangularRoom-3']
+    ]
+
+    @staticmethod
+    def generate_test(layout):
+        def test(self):
+            args = pacman.readCommand(['-p', 'RectangularRoomCleaner', '-l', layout, '-q'])
+            games = pacman.runGames(**args)
+            self.assertTrue(games[0].state.isWin())
+
+        return test
+
+    @staticmethod
+    def setup_tests():
+        for test in TestRectangularRoomCleaner.layouts:
+            test_name = 'test_%s' % test[0]
+            func = TestRectangularRoomCleaner.generate_test(test[0])
+            setattr(TestRectangularRoomCleaner, test_name, func)
+
+
+TestRectangularRoomCleaner.setup_tests()
+if __name__ == '__main__':
+    unittest.main()
