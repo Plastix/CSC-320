a42ed7fc954554b18171febde9da8027579618a4
Plastix <aidanpieper@gmail.com>
2018-04-04 14:09:08 -0400

    Begin work on RectangularRoomCleaner

--- pacmanAgents.py
+++ pacmanAgents.py
@@ -19,23 +19,25 @@ from pacman import Directions
 from game import Agent
 import random
 
-class GoWestAgent(Agent):
-    "An agent that goes West until it can't."
 
-    def getAction(self, gameState):
+class GoWestAgent(Agent):
+    """An agent that goes West until it can't."""
+
+    def getAction(self, game_state):
         "The agent receives a GameState (defined in pacman.py)."
-        if Directions.WEST in gameState.getLegalPacmanActions():
+        if Directions.WEST in game_state.getLegalPacmanActions():
             return Directions.WEST
         else:
             return Directions.STOP
-        
-class LeftTurnAgent(Agent):
-    "An agent that turns left at every opportunity"
 
-    def getAction(self, gameState):
-        legal = gameState.getLegalPacmanActions()
-        
-        current = gameState.getPacmanState().getDirection()
+
+class LeftTurnAgent(Agent):
+    """An agent that turns left at every opportunity"""
+
+    def getAction(self, game_state):
+        legal = game_state.getLegalPacmanActions()
+
+        current = game_state.getPacmanState().getDirection()
         if current == Directions.STOP:
             current = Directions.NORTH
 
@@ -51,6 +53,39 @@ class LeftTurnAgent(Agent):
             return Directions.STOP
 
 
-##########################
-### Add your code here ###
-##########################
+class RectangularRoomCleaner(Agent):
+    """
+    A simple-reflex agent that will east an entire rectangular room. Assumes that there are no obstacles.
+    """
+
+    def getAction(self, game_state):
+        legal = game_state.getLegalPacmanActions()
+        current = game_state.getPacmanState().getDirection()
+        left = Directions.LEFT[current]
+        right = Directions.RIGHT[current]
+
+        if current == Directions.STOP:
+            current = Directions.WEST
+
+        # Turn south when hitting a wall
+        if current not in legal and left in legal and right in legal:
+            return left if current == Directions.WEST else right
+
+        # Turn east after hitting west wall
+        if current == Directions.SOUTH and left in legal and right not in legal:
+            return left
+
+        # Turn west after hitting east wall
+        if current == Directions.SOUTH and left not in legal and right in legal:
+            return right
+
+        # Turn right when hitting a corner
+        if current not in legal and (left not in legal or right not in legal):
+            return right
+
+        # Go straight if possible
+        if current in legal:
+            return current
+
+        # Fail-safe to stop errors
+        return Directions.STOP
